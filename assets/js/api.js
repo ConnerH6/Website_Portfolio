1ca0d0a37b5b226c6b8152aa10782d5765bf7a88
ConnerH6 <connerhoff@gmail.com>
2022-02-13 11:47:37 -0500

    api config

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,26 +1,13 @@
-const countURL = 'https://ryb7z9tjek.execute-api.us-east-1.amazonaws.com/Count'
+const countUrl = 'https://ryb7z9tjek.execute-api.us-east-1.amazonaws.com/Count'
+const countElement = document.getElementById('count');
 
-getVisitors();
-function getVisitors() {
-  return fetch("https://ryb7z9tjek.execute-api.us-east-1.amazonaws.com/Count",
-  {
-    method: "GET",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-  'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*'
-    },
-  })
-  .then((response) => response.json())
-  .then((responseData) => {
-    console.log(responseData);
-    return responseData;
-  })
-  .catch(error => console.warn(error));
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch(countUrl)
+        .then(res => res.json())
+        .then(res => {
+        countElement.innerHTML = res.visits;
+    });
 }
-
-
-
 
